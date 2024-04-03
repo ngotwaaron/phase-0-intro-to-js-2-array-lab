0537120baa9dc2ebb4d6ab26ce9c4be6b88766dd
ngotwaaron <ngotwaaron@gmail.com>
2024-04-03 14:32:12 +0300

    phase-0-intro to js -2 arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+
+};
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+
+};
+
+function appendCat(name) {
+    const copyOfCats = [...cats]; 
+    copyOfCats.push(name)
+
+    return(copyOfCats)
+};
+
+function prependCat(name) {
+    const copyOfCats = [...cats]; 
+    copyOfCats.unshift(name)
+
+    return(copyOfCats)
+};
+
+function removeLastCat(name) {
+    const copyOfCats = [...cats]; 
+    copyOfCats.pop()
+
+    return(copyOfCats)
+};
+
+function removeFirstCat(name) {
+    const copyOfCats = [...cats]; 
+    copyOfCats.shift()
+
+    return(copyOfCats)
+};
